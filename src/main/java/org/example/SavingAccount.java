9d8a7473140aec80552baba3d58b84fb57c0e1a4
Stuart <stuartmcmillan100@hotmail.com>
2022-11-01 14:58:51 +0000

    Another test

--- src/main/java/org/example/SavingAccount.java
+++ src/main/java/org/example/SavingAccount.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class SavingAccount extends BankAccount {
 
-    // Another git test.
+    // Test to see if it'll run.
     private String accountType;
     private double interestRate;
 
